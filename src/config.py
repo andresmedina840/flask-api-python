23b7afb0e6e7aaf93887d8fda70a214ef314e112
andresmedina840 <andresmedina840@hotmail.com>
2023-04-04 16:59:17 -0500

    Actualizacion

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = config('SECRET_KEY')
 
 class DevelopmentConfig(Config):
-    DEBUG = True
+    DEBUG = False
     DB_SERVER = '150.136.169.25'
 
 class ProductionConfig(Config):
